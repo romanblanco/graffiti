dda5f6eeebc9af55076f3cca4cf4b8118b3b84bb
Roman Blanco <roman.blanco92@gmail.com>
2019-06-16 20:04:53 +0000

    osm surface

--- map.rb
+++ map.rb
@@ -12,48 +12,50 @@ set :public_folder, Proc.new { File.join(root, "assets/photos/") }
 
 get '/' do
   erb :index, :locals => {
-    :data => photo_gps_list,
+    :data => photo_gps_list('all'),
     :token => settings.token
   }
 end
 
-get '/detail' do
+get '/detail/?:region?' do |r|
   erb :detail, :locals => {
-    :data => photo_gps_list,
+    :data => photo_gps_list(params['region']),
     :token => settings.token
   }
 end
 
-get '/export/:region' do |r|
+get '/api/?:region?' do |r|
   json(export(params['region']))
 end
-
 
 get "/assets/photos/:file" do |file|
   send_file File.join(File.absolute_path('assets/photos/') + '/' + file)
 end
 
-def photo_gps_list
+def photo_gps_list(region)
   table = CSV.open("assets/graffiti.csv", headers: true).map(&:to_h)
   olc = PlusCodes::OpenLocationCode.new
   photos_url = '/assets/photos/'
+  all = region == "all"
   result = table.map do |photo|
+    plus_code = olc.encode(Float(photo["latitude"]), Float(photo["longitude"]), 16)
     {
       type: 'Feature',
       "geometry": { "type": "Point", "coordinates": [photo["longitude"], photo["latitude"]]},
       "properties": {
         "image": File.join(photos_url, photo["filename"]),
         "ipfs": photo["ipfs"],
+        "surface": photo["surface"],
         "url": File.join(photos_url, photo["filename"]),
         "date": photo["date"],
         "gps_longitude": photo["longitude"],
         "gps_latitude": photo["latitude"],
-        "plus": olc.encode(Float(photo["latitude"]), Float(photo["longitude"]), 16),
+        "plus": plus_code,
         "marker-symbol": "art-gallery",
-        "marker-color": "#000000",
+        "marker-color": photo["surface"] != nil ? "#00FF00" : "#000000",
         "marker-size": "medium",
       }
-    }
+    } if all or plus_code.match("^#{region}")
   end
   result.compact.to_json
 end
@@ -64,7 +66,12 @@ def export(region)
   olc = PlusCodes::OpenLocationCode.new
   table = CSV.open("assets/graffiti.csv", headers: true).map(&:to_h)
   table.map { |photo|
-    photo["plus_code"] = olc.encode(Float(photo["latitude"]), Float(photo["longitude"]), 16)
+    latitude = Float(photo["latitude"])
+    longitude = Float(photo["longitude"])
+    photo["plus_code"] = olc.encode(latitude, longitude, 16)
+    photo["latitude"] = latitude
+    photo["longitude"] = longitude
+    photo["local"] = "/assets/photos/#{photo["filename"]}"
     photo["url"] = "https://ipfs.io/ipfs/#{photo["ipfs"]}"
   }
   table.map { |record| record if all or record["plus_code"].match(starts_with_region) }.compact
